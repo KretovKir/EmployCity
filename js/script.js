ee31ed51a1952824654cbe673894f1ea457f0332
KretovKir <KretovKir526@mail.ru>
2025-03-11 23:01:10 +0300

    create mobile menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 const rangeElem = document.getElementById('rangeElem');
-const counter = document.getElementById('counter')
+const counter = document.getElementById('counter');
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.mobile-menu');
+const cross = document.querySelector('.cross');
 
 document.getElementById("rangeElem").addEventListener("change", function() {
   counter.textContent = this.value + "%"
   console.log(this.value);
 });
 
+
+burger.addEventListener('click', ()=>{
+  document.body.classList.add('overflow-hidden');
+  menu.classList.toggle('active');
+});
+
+cross.addEventListener('click', ()=>{
+  menu.classList.remove('active');
+  document.body.classList.remove('overflow-hidden')
+})
